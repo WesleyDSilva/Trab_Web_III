8dca9596a8977ed049327e6608aa69c728f47efb
WesleyDSilva <wdias00@gmail.com>
2023-12-08 18:13:57 -0300

    Update validacao.php

--- js/validacao.php
+++ js/validacao.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $host = "devweb2sql.mysql.dbaas.com.br";
 $usuario = "devweb2sql";
 $senha = "g2023_FaTEC#$";
@@ -6,34 +8,41 @@ $nome_do_banco = "devweb2sql";
 
 $conexao = new mysqli($host, $usuario, $senha, $nome_do_banco);
 
+// Verifique se há falha na conexão
 if ($conexao->connect_error) {
     die("Falha ao conectar ao banco de dados: " . $conexao->connect_error);
 }
 
-session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Require_once deve incluir o arquivo de conexão atual, não o "conexaoTeste.php"
-    require_once("conexaoTeste.php");
+    $telefone = $_POST["telefone"];
+    $senha = $_POST["senha"];
 
-    $login = $_POST["TELEFONE_CLI"];
-    $senha = $_POST["SENHA_CLI"];
+    // Use prepared statements para evitar SQL injection
+    $stmt = $conexao->prepare("SELECT * FROM usuarios WHERE telefone = ? AND senha = ?");
+    
+    // Verifique se a preparação foi bem-sucedida
+    if ($stmt) {
+        $stmt->bind_param("ss", $telefone, $senha);
+        $stmt->execute();
 
-    $login = $conexao->real_escape_string($login);
-    $senha = $conexao->real_escape_string($senha);
+        // Obtenha o resultado
+        $resultado = $stmt->get_result();
 
-    $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
-    $resultado = $conexao->query($sql);
+        // Verifique se a consulta retornou exatamente 1 linha
+        if ($resultado->num_rows == 1) {
+            $usuario = $resultado->fetch_assoc();
+            $_SESSION["id"] = $usuario["id"];
+            $_SESSION["nome"] = $usuario["nome"];
+            header("Location: index.php");
+            exit();
+        } else {
+            $erro = "Telefone ou senha incorretos.";
+        }
 
-    if ($resultado->num_rows == 1) {
-        $usuario = $resultado->fetch_assoc();
-        $_SESSION["id"] = $usuario["id"];
-        $_SESSION["NOME_CLI"] = $usuario["NOME_CLI"];
-        header("Location: index.php");
-        exit();
+        // Feche o statement
+        $stmt->close();
     } else {
-        $erro = "Usuário ou senha incorretos.";
+        die("Erro na preparação da consulta: " . $conexao->error);
     }
 }
-
 ?>
